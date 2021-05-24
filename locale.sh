7e1cb2b2a8cdd166c45cee9d95c13f105e72ed4a
Michael Lefkowitz <1746853+lfkwtz@users.noreply.github.com>
2021-05-24 06:17:39 -0500

    Update locale.sh

--- locale.sh
+++ locale.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
 sudo grep ^en_US\.UTF-8 /usr/share/i18n/SUPPORTED > /var/lib/locales/supported.d/local
+
 sudo dpkg-reconfigure locales
+
 sudo dpkg-reconfigure keyboard-configuration
+
 sudo echo "America/Chicago" > /etc/timezone
+
 sudo dpkg-reconfigure -f noninteractive tzdata
+
 sudo sed -i '/ftp.uk.debian.org/s/uk/us/g' /etc/apt/sources.list
+
 sudo apt-get update
+
 sudo reboot
